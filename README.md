b03c8d4597dcceca7f637e12470d4d4a61ab869d
Puritanic <darktasevski@gmail.com>
2018-05-03 19:41:19 +0200

    Add js logic and styles

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-frontend-nanodegree-arcade-game
-===============================
+# Classic Arcade Game
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+> Udacity Front End Nanodegree project
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Built with Javascript.
+
+## Instructions
+
+Use the arrow keys to move the player.Player should avoid bugs(enemies),and reach the water.Every time the player is hit by a bug,
+it loses a life. When all the hearts are lost,the game ends. Once the player collects 1000 points he wins.
+
+## Getting started
+
+After cloning or downloading the project, open index.html with your favorite browser and enjoy game :)
--- js/app.js
+++ js/app.js
@@ -1,46 +1,128 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+const enemies = [50, 145, 210, 145, 50, 200];
+const points = document.getElementById('points');
+const modal = document.getElementById('gameOverModal');
+const winModal = document.getElementById('winningModal');
+const hearts = document.getElementsByClassName('lives')[0];
+let score = 0;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+// Open modal in case of defeat
+const openModal = () => {
+  modal.style.display = 'flex';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+// Opens victory modal once player win game
+const openVictoryModal = () => {
+  winModal.style.display = 'flex';
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// Closes modal, starts new game
+const modalClose = () => {
+  modal.style.display = 'none';
+  window.location.reload();
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Listen for click event
+modal.addEventListener('click', modalClose);
+winModal.addEventListener('click', modalClose);
 
+class Game {
+  constructor(x, y, image) {
+    this.x = x;
+    this.y = y;
+    this.image = image;
+  }
+  render() {
+    window.ctx.drawImage(window.Resources.get(this.image), this.x, this.y);
+  }
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Player class
+class Player extends Game {
+  constructor(x, y, image, lives) {
+    super(x, y, image, lives);
+    this.lives = lives;
+  }
 
+  update() {
+    // adds 100 points each time the player reaches the water.If the score reaches 1000 points he wins
+    if (this.y < 40) {
+      this.x = 200;
+      this.y = 380;
+      score += 100;
+      points.innerText = score;
+      if (score === 1000) {
+        openVictoryModal();
+      }
+    }
+  }
+  // this function is for player's movement using the up,down,left and right keys
+  handleInput(key) {
+    if (key === 'left' && this.x > 0) this.x -= 100;
+    else if (key === 'right' && this.x < 400) this.x += 100;
+    else if (key === 'up') this.y -= 80;
+    else if (key === 'down' && this.y < 380) this.y += 80;
+  }
 
+  // Checks for collisions, if collision removes heart, if no hearts defeat
+  checkLives(arr) {
+    this.lives -= 1;
+    hearts.removeChild(hearts.children[0]);
+    if (this.lives === 0) {
+      this.image = 'images/ghost.png';
+      arr.forEach((enemy) => {
+        enemy.speed = 0;
+      });
+      openModal();
+    }
+  }
+}
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+// Instantiate new player
+const player = new Player(200, 380, 'images/char-boy.png', 3);
 
-    player.handleInput(allowedKeys[e.keyCode]);
+// player movement keys handler
+document.addEventListener('keyup', (e) => {
+  const allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Enemy class
+class Enemy extends Game {
+  constructor(x, y, image, speed) {
+    super(x, y, image, speed);
+    this.speed = speed;
+  }
+
+  /* Every time a bug reaches the end of canvas, it starts from the new random point with different speed */
+  update(dt) {
+    if (this.x < 505) {
+      this.x += dt * 15 * this.speed * Math.random();
+    } else {
+      const i = Math.floor(Math.random() * enemies.length) + 1;
+      this.y = enemies[i];
+      this.x = -100;
+    }
+
+    // collisions check
+    // https://stackoverflow.com/q/2440377
+    const a = this.x - player.x;
+    const b = this.y - player.y;
+    const c = 70;
+    if (a * a + b * b <= c * c) {
+      player.x = 200;
+      player.y = 380;
+      player.checkLives(allEnemies);
+    }
+  }
+}
+
+// Instantiate enemies
+const enemy1 = new Enemy(-100, 140, 'images/enemy-bug.png', 50);
+const enemy2 = new Enemy(-100, 60, 'images/enemy-bug.png', 20);
+const enemy3 = new Enemy(-100, 220, 'images/enemy-bug.png', 30);
+const allEnemies = [enemy1, enemy2, enemy3];
